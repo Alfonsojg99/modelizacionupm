f7e4a975c1cf1fbbf0140c1ac300f8712af69b78
Javier <j.herraez@alumnos.upm.es>
2020-05-29 17:35:59 +0200

    www

--- ajederez2.py
+++ ajederez2.py
@@ -20,17 +20,6 @@ ocupadas=[
 [0,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0,0]
 ]
-cocupadas=[#color de las ocupadas
-[0,0,0,0,0,0,0,0,0],#esta linia y el 0 de mas es para kitar el 0 de los indices hola qu etal 
-[0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0]
-]
 
 Rey = [[2, 1, 2, 1, 2, 1, 2, 1],
        [1, 0, 3, 0, 3, 0, 3, 0],
@@ -50,19 +39,17 @@ Torre = [[0, 1, 2, 3, 4, 5, 6, 7],
          [7, 6, 5, 4, 3, 2, 1, 0]]
 
 class metapieza():
-	def __init__(self,x,y,color):
+	def __init__(self,x,y):
 		self.movida = 0
 		self.casx=x
 		self.casy=y
 		self.pos=(casilla[x],casilla[y])
-		self.color=color
 		self.casposibles=[]
 		if self.casx < 9 and self.casy < 9:
 			ocupadas[self.casy][self.casx] = self
-			cocupadas[self.casy][self.casx] = self.color
 
 	def cambiasilla(self,x,y):
-		ocupadas[self.casy][self.casx]=cocupadas[self.casy][self.casx] = 0
+		ocupadas[self.casy][self.casx] = 0
 		self.__init__(x,y)
 		self.movida = 1
 
@@ -72,22 +59,20 @@ class metapieza():
 		while casi < movmax:
 			casi+=1
 			if 0 < self.casy <= 8 and 0 < self.casx+casi <= 8 and ordr:
-					self.casposibles.append((self.casx+casi,self.casy))
-					ordr = cocupadas[self.casy][self.casx+casi] != 3-self.color				
-
+					ordr = ocupadas[self.casy][self.casx + casi] == 0
+					if ordr:
+						self.casposibles.append((self.casx+casi,self.casy))
 			if 0 < self.casy+casi <= 8 and 0 < self.casx <= 8 and vrar:
-					self.casposibles.append((self.casx,self.casy+casi))
-					vrar = cocupadas[self.casy+casi][self.casx] != 3-self.color
+					vrar = ocupadas[self.casy + casi][self.casx] == 0
+					if vrar:
+						self.casposibles.append((self.casx,self.casy+casi))
 		return self.casposibles
-		
-	def movdiagonal(self,movmax=8):
-		casi = 0
-		abdr  = True
-		while casi < movmax:
-			casi+=1
-			if 0 < self.casy+casi <= 8 and 0 < self.casx+casi <= 8 and abdr:
-					self.casposibles.append((self.casx+casi,self.casy+casi))
-					abdr = cocupadas[self.casy+casi][self.casx+casi] != 3-self.color
+
+	def movdiagonal(self):
+		casi = 1
+		abdr = ocupadas[self.casy + casi][self.casx + casi] == 0
+		if 0 < self.casy + casi <= 8 and 0 < self.casx + casi <= 8 and abdr:
+			self.casposibles.append((self.casx + casi, self.casy + casi))
 		return self.casposibles
 
 
@@ -95,27 +80,30 @@ class metarey(metapieza):
 	def movrey(self):
 		posimov=[]
 		posimov+=metapieza.movlineal(self,1)
-		posimov+=metapieza.movdiagonal(self,1)
+		posimov+=metapieza.movdiagonal(self)
 		return posimov
 
 class Torreblanca(metapieza):
 	def __init__(self,x=3,y=1):
+		self.nombre = "torre"
 		self.foto = pygame.image.load('torreblanca.png')
-		metapieza.__init__(self,x,y,1)
+		metapieza.__init__(self,x,y)
 	def puedemovera(self):
-		return metapieza.movlineal(self)	
+		return metapieza.movlineal(self)
 
 class Reyblanco(metarey):
 	def __init__(self,x=1,y=2):
+		self.nombre = "rey"
 		self.foto = pygame.image.load('reyblanco.png')
-		metapieza.__init__(self,x,y,1)
+		metapieza.__init__(self,x,y)
 	def puedemovera(self):
 		return metarey.movrey(self)
 
 
 def	sacapieza(casx,casy):
 	return ocupadas[casy][casx]
-		
+
+
 def sacasilla(posraton):
 	for i in range(9):
 		if casilla[i] < posraton[0] <= casilla[i+1]:
@@ -125,7 +113,7 @@ def sacasilla(posraton):
 	return x,y
 
 
-tablero = pygame.image.load('tablero-ajedrez.png')			
+tablero = pygame.image.load('tablero-ajedrez.png')
 puntoazul = pygame.image.load('puntoazul.png')
 gblancas = pygame.image.load('gblancas.png')
 gnegras = pygame.image.load('gnegras.png')
@@ -165,6 +153,7 @@ while True:
 			posraton = click[1]
 			nuevacasillax,nuevacasillay = sacasilla(posraton)
 			if (nuevacasillax,nuevacasillay) in posimov:
+				print(nuevacasillax)
 				fichamover.cambiasilla(nuevacasillax,nuevacasillay)
 				turno = 2
 
@@ -172,20 +161,20 @@ while True:
 
 		visor.blit(torreblanca.foto,torreblanca.pos)
 		visor.blit(reyblanco.foto,reyblanco.pos)
-		
-	
+
+
 		if len(click) > 1:
 			click=[]
 			fichamover=""
 		if len(click) > 0:
 			for pos in posimov:
 				visor.blit(puntoazul,(casilla[pos[0]],casilla[pos[1]]))
-            
+
 	#elif reyblanco.casx > 8:
 	#	visor.blit(gnegras,(0,0))
 
 		pygame.display.update()
-	
+
 	#pygame.time.wait(20)#limita a 50 fps para ahorrar cpu
 
 	if turno == 2:
@@ -195,20 +184,42 @@ while True:
 		for i in range(len(ocupadas)):
 			for j in range(len(ocupadas[i])):
 				if ocupadas[i][j] != 0 :
-					lista.append(ocupadas[i][j])
-					posX.append(i)
-					posY.append(j)
+					lista.append(ocupadas[i][j].nombre)
+					posX.append(j)
+					posY.append(i)
+
 		print(lista)
 		print(posX)
 		print(posY)
-		valorTorre = Torre[posX[1]-1][posY[1]-1]
-		valorRey = Rey[posX[0]-1][posY[0]-1]
+		if lista[0] == "rey":
+			valorRey = Rey[posX[0] - 1][posY[0] - 1]
+			valorTorre = Torre[posX[1] - 1][posY[1] - 1]
+			fichamoverRey = sacapieza(posX[0], posY[0])
+			posimovRey = fichamoverRey.puedemovera()
+			fichamoverTorre = sacapieza(posX[1], posY[1])
+			posimovTorre = fichamoverTorre.puedemovera()
+		else:
+			valorRey = Rey[posX[1] - 1][posY[1] - 1]
+			valorTorre = Torre[posX[0] - 1][posY[0] - 1]
+			fichamoverRey = sacapieza(posX[1], posY[1])
+			posimovRey = fichamoverRey.puedemovera()
+			fichamoverTorre = sacapieza(posX[0], posY[0])
+			posimovTorre = fichamoverTorre.puedemovera()
 		valorNim = sumdig([valorRey, valorTorre])
 		print(valorNim)
-		for i in range(len(Torre)):
-			for j in range(len(Torre[i])):
-				if ocupadas[i][j] != 0 :
-
+		print(posimovRey)
+		print(posimovTorre)
+		sincambiar = True
+		for (nuevacasillax,nuevacasillay) in posimovTorre:
+			if sincambiar:
+				print (casilla)
+				nuevovalorTorre = Torre[nuevacasillax-1][nuevacasillay-1]
+				sumav = sumdig([valorRey, nuevovalorTorre])
+				if sumav > 0:
+					print(casilla[0])
+					print(casilla[1])
+					fichamoverTorre.cambiasilla(nuevacasillax,nuevacasillay)
+					sincambiar = False
 
 		turno = 1
 
